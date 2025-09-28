d32925ff8c6af04db017107d41bc615a26a941df
sheraz <sherazyousaf@hotmail.com>
2025-09-28 02:10:15 +0100

    Update ScenarioSelector.js

--- frontend/js/components/ScenarioSelector.js
+++ frontend/js/components/ScenarioSelector.js
@@ -62,15 +62,12 @@ class ScenarioSelector {
     handleStopDemo() {
         const demoManager = window.appController?.getManager('demo');
         const toastManager = window.appController?.getManager('toast');
-
+        
         if (demoManager) {
             window.appController.stopDemo();
             toastManager?.addToast('Demo stopped', 'info');
-            this.loading = null;  // Reset loading state
-            this.render();  // Force a render to update the UI
         }
     }
-
 
     handleResetDemo() {
         const demoManager = window.appController?.getManager('demo');
